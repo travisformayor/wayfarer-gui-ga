a8c2dd140c1559c4033cbf06e4441fa948312961
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-05-03 15:59:30 -0700

    Added basic userPostEndpoint to get all posts

---
+++ src/models/userPost.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const userPostEndpoint = `http://localhost:4000/api/v1/posts`;
+
+class PostModel {
+  // Static method to be called
+  static all() {
+    const api_url = `${userPostEndpoint}/all`;
+    let request = axios.get(api_url);
+    return request;
+  }
+
+  // static createPost(userPost) {
+  //   const api_url = `${userPostEndpoint}/createPost`;
+  //   let request = axios.post(api_url);
+  //   return request;
+  // }
+
+  // static deletePost(userPost) {
+  //   const api_url = `${userPostEndpoint}/deletePost`;
+  //   let request = axios.delete(api_url);
+  //   return request;
+  // }
+
+  // static updatePost(userPost) {
+  //   const api_url = `${userPostEndpoint}/updatePost`;
+  //   let request = axios.put(api_url);
+  //   return request;
+  // }
+}
+
+
+
+export default PostModel;

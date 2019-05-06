9679d5831bbb17545312be0bb64648886c957621
Travis <travisformayor@users.noreply.github.com>
2019-05-06 00:20:08 -0700

    added images to City page

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Posts from '../Posts/Posts';
+import Seattle from '../../img/seattle.jpeg';
+import SF from '../../img/san-francisco.jpg';
+import Sydney from '../../img/sydney.jpg';
+import London from '../../img/london.jpg';
 
 class City extends Component {
 
-  
   render () {
 
     let { cities, allPosts, currentCity } = this.props;
@@ -24,10 +27,34 @@ class City extends Component {
     }
     // console.log('Current city posts', cityPosts);
 
+    let image;
+    switch (currentCity) {
+      case 'san-francisco':
+        image = <img class="responsive-img" src={SF} alt="San Francisco" />;
+        break;
+      case 'seattle':
+        image = <img class="responsive-img" src={Seattle} alt="Seattle" />;
+        break;
+      case 'london':
+        image = <img class="responsive-img" src={London} alt="London" />;
+        break;
+      case 'sydney':
+        image = <img class="responsive-img" src={Sydney} alt="Sydney" />;
+        break;
+      default:
+        image = <img class="responsive-img"  alt="Error Loading Image" />;
+    }
+
       return (
       <div className="col s9">
+        <div className="city-header">
         <h1>{city && city.cityName}</h1>
         <h1>{city && city.country}</h1>
+        </div>
+        <div className="city-img">
+          {image}
+        </div>
+        
 
         <div className="posts-holder">
           <Posts posts={cityPosts} />

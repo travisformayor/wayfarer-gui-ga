43113b5f04607961c8fe0a4faefa089b474a8670
Ridwan sarwari <r.sarwari@vigilvideo.com>
2019-05-06 01:01:05 -0700

    css updated for landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import logo from './home-goldengate.jpg';
+import logo from './home-goldengate.png';
 
 class Home extends Component {
   render() {
+    let height = {
+      height: 400
+    }
     return (
       // <h1>
       //   Wayfarer Landing Page
       // </h1>
-      <div class="row">
-    <div class="col s12 m12" >
-      <div class="card">
-        <div class="card-image">
+      <div className="row">
+      <div className="col s12 m12" >
+        <div className="center">
         <img src={logo}/>
-          
-        </div>
-        
-        
+        <div class="row">
+
+      <div class="col s4">
+        <h5>Topic 1</h5>
+        <p> Lorem ipsum dolor sit amet, eu quod quot ipsum vel, id eum singulis postulant. Eum te numquam facilisi intellegebat. Ius no ipsum eloquentiam. Oblique propriae qui te, legere periculis no his.  </p>
       </div>
+      <div class="col s4">
+        <h5>Topic 2</h5>
+        <p>Lorem ipsum dolor sit amet, eu quod quot ipsum vel, id eum singulis postulant. Eum te numquam facilisi intellegebat. Ius no ipsum eloquentiam. Oblique propriae qui te, legere periculis no his.</p>
+      </div>
+      <div class="col s4">
+        <h5>Topic3</h5>
+        <p>Lorem ipsum dolor sit amet, eu quod quot ipsum vel, id eum singulis postulant. Eum te numquam facilisi intellegebat. Ius no ipsum eloquentiam. Oblique propriae qui te, legere periculis no his.</p>
+      </div>
+
+    </div>
+        
+        
+        </div>
+      
     </div>
   </div>
     );

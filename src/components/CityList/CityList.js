c650f9825c2391d1393f39bbb01db23adc0dd07c
Ridwan sarwari <r.sarwari@vigilvideo.com>
2019-05-06 10:28:54 -0700

    centred text in citylist

--- src/components/CityList/CityList.js
+++ src/components/CityList/CityList.js
@@ -5,17 +5,27 @@ class CityList extends Component {
   render () {
     let { cities } = this.props;
 
+    const cityHeight = {
+      height: 150,
+    }
+     
     return (
       <div className="col s3">
+      <div className="valign">
+      <div className="center-align">
         {cities.map(city => (
+          <div style={cityHeight}>
           <CityListItem
             key={city._id}
             cityName={city.cityName}
             cityURL={city.cityURL}
             currentCity={this.props.currentCity}
           />
+          </div>
         ))
         }
+        </div>
+        </div>
       </div>
     )
   }

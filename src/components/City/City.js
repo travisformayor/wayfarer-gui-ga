7e5d77857d7f2cef33c8261db1418e59b3f5bbbb
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-05-05 23:12:11 -0700

    Modal populates with basic form. Needs proper styling

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import CreatePost from '../Modals/CreatePost';
+import PostModal from '../Modals/PostModal';
 
 class City extends Component {
+  // Version 1
+  // state = {
+  //   show: false,
+  // };
+
+  // version 1 - not working yet
+  // showModal = () => {
+  //   this.setState({show: true});
+  // }
+  // hideModal = () => {
+  //   this.setState({show: false});
+  // }
+
+  // Version 2
   state = {
-    show: false,
+    creating: false
   };
 
-  showModal = () => {
-    this.setState({show: true});
-  }
-
-  hideModal = () => {
-    this.setState({show: false});
+  createNewPostHandler = () => {
+    this.setState({creating: true})
   }
 
   render () {
@@ -24,28 +35,39 @@ class City extends Component {
         return city.cityURL === currentCity;
       });
     }
-    // console.log('found city ', city)
 
       return (
-<<<<<<< HEAD
         <div className="col s9">
           <h3>{city.cityName}</h3>
           <h4>{city.country}</h4>
 
-          <span className="span-button">
+          {/* <span className="span-button">
 
-            <button data-target="modal1" onCLick={this.showModal}>
-              <a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">+</i></a>
-            </button>
+          </span> */}
 
-          </span>
+          {/* <button data-target="modal1" className="btn modal-trigger">Modal</button> */}
+          {/* <div id="modal1" className="modal">
+            <div className="modal-content">
+              <h4>Modal Header</h4>
+              <p>A bunch of text</p>
+            </div>
+            <div className="modal-footer">
+              <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+            </div>
+          </div> */}
+
+          {this.state.creating && (
+            <PostModal title="Create a new post" canCancel canConfirm>
+            <p>Modal Content</p>
+            </PostModal>
+          )}
+
+          <button data-target="modal1" onClick={this.createNewPostHandler}>
+            <a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">+</i></a>
+          </button>
+
+
         </div>
-=======
-      <div className="col s9">
-        <h1>{city && city.cityName}</h1>
-        <h1>{city && city.country}</h1>
-      </div>
->>>>>>> dev
       )
   }
 }
--- src/components/Modals/PostModal.js
+++ src/components/Modals/PostModal.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const postModal = props => (
   <div>
+    <header>{props.title}</header>
+    <section className="modal__content">
+      {props.children}
+    </section>
+    <section className="modal__actions">
+      {props.canPost && <button>Post</button>}
+      {props.canCancel && <button>Cancel</button>}
+    </section>
 
   </div>
 );
